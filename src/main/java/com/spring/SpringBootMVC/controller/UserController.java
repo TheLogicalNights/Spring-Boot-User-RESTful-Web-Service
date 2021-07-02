8cd44d73dad02049f5689b2d502c1d898d4a98b9
TheLogicalNights <swapniladhav560@gmail.com>
2021-07-02 13:34:43 +0530

    Added createUser() into controller

--- src/main/java/com/spring/SpringBootMVC/controller/UserController.java
+++ src/main/java/com/spring/SpringBootMVC/controller/UserController.java
@@ -1,26 +1,48 @@
 package com.spring.SpringBootMVC.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.SpringBootMVC.model.UserModel;
 
 @RestController
 @RequestMapping("/users")
 public class UserController 
 {
-	@GetMapping("/{userId}")
+	Map<String, UserModel> user;
+	@GetMapping(path="/{userId}")
 	public String getUser(@PathVariable String userId)
 	{
 		return "getUser() was called with userId = " + userId;
 	}
 	
-	@PostMapping
-	public String createUser()
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE,
+							 MediaType.APPLICATION_XML_VALUE})
+	public ResponseEntity<String> createUser(@RequestBody UserModel studentObj)
 	{
-		return "createUser() was called";
+		UserModel returnValue = new UserModel();
+		returnValue.setUserId(studentObj.getUserId());
+		returnValue.setFirstName(studentObj.getFirstName());
+		returnValue.setLastName(studentObj.getLastName());
+		returnValue.setEmail(studentObj.getEmail());
+		
+		if(user==null)
+		{
+			user = new HashMap<>();
+		}
+		user.put(returnValue.getUserId(), returnValue);
+		return new ResponseEntity<String>("User created successfully",HttpStatus.OK);
 	}
 	
 	@PutMapping
