f98eb7adf739c06c34a07ed2cc34bab2f3c372a9
TheLogicalNights <swapniladhav560@gmail.com>
2021-07-02 15:03:07 +0530

    Added new service updateUser

--- src/main/java/com/spring/SpringBootMVC/services/UserService.java
+++ src/main/java/com/spring/SpringBootMVC/services/UserService.java
@@ -1,8 +1,12 @@
 package com.spring.SpringBootMVC.services;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
 import com.spring.SpringBootMVC.model.UserModel;
 
 public interface UserService 
 {
 	UserModel createUser(UserModel userObj);
+	UserModel updateUser(String userId,UserModel userDetails);
 }
