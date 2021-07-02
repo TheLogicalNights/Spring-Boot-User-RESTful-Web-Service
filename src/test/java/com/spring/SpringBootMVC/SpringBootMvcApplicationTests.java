af88f34dd31250c7d6afbdb17558b835b0bde2c6
TheLogicalNights <swapniladhav560@gmail.com>
2021-07-02 13:14:50 +0530

    Initial commit

---
+++ src/test/java/com/spring/SpringBootMVC/SpringBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.SpringBootMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
