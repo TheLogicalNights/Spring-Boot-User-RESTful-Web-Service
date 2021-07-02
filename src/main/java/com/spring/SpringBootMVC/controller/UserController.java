6ba1a7e011551ff01ca3108aa8beb39a0e2871a0
TheLogicalNights <swapniladhav560@gmail.com>
2021-07-02 13:18:02 +0530

    Added post handler into controller

--- src/main/java/com/spring/SpringBootMVC/controller/UserController.java
+++ src/main/java/com/spring/SpringBootMVC/controller/UserController.java
@@ -1,5 +1,24 @@
 package com.spring.SpringBootMVC.controller;
 
-public class UserController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/users")
+public class UserController 
+{
+	@GetMapping("/{userId}")
+	public String getUser(@PathVariable String userId)
+	{
+		return "getUser() was called with userId = " + userId;
+	}
+	
+	@PostMapping
+	public String createUser()
+	{
+		return "createUser() was called";
+	}
 }
