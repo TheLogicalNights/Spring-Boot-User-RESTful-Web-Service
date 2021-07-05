51cbf22edf5fba6e51b2160979a0aa6b65be7fa8
TheLogicalNights <swapniladhav560@gmail.com>
2021-07-05 15:14:34 +0530

    Added new services

--- src/main/java/com/spring/SpringBootMVC/services/UserService.java
+++ src/main/java/com/spring/SpringBootMVC/services/UserService.java
@@ -9,7 +9,7 @@ public interface UserService
 	public List<UserModel> getUsers();
 	public UserModel getUser(int userId);
 	public Boolean createUser(UserModel userObj);
-	public Boolean updateUser(String userId,UserModel userDetails);
+	public Boolean updateUser(int userId,UserModel userDetails);
 	public Boolean deleteUsers();
 	public Boolean deleteUser(int userId);
 }
