fa908d4594f5fac10093cda6a52b02a6c47e663b
TheLogicalNights <swapniladhav560@gmail.com>
2021-07-05 14:50:40 +0530

    Added new Services

--- src/main/java/com/spring/SpringBootMVC/services/UserService.java
+++ src/main/java/com/spring/SpringBootMVC/services/UserService.java
@@ -1,9 +1,15 @@
 package com.spring.SpringBootMVC.services;
 
+import java.util.List;
+
 import com.spring.SpringBootMVC.model.UserModel;
 
 public interface UserService 
 {
-	UserModel createUser(UserModel userObj);
-	UserModel updateUser(String userId,UserModel userDetails);
+	public List<UserModel> getUsers();
+	public UserModel getUser(int userId);
+	public Boolean createUser(UserModel userObj);
+	public Boolean updateUser(String userId,UserModel userDetails);
+	public Boolean deleteUsers();
+	public Boolean deleteUser(int userId);
 }
