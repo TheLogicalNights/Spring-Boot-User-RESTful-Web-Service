75352b6c77a5503f4d1a2705f0b7e591a64dea17
TheLogicalNights <swapniladhav560@gmail.com>
2021-07-05 14:50:12 +0530

    Updated

--- src/main/java/com/spring/SpringBootMVC/repository/UserRepository.java
+++ src/main/java/com/spring/SpringBootMVC/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.spring.SpringBootMVC.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
 
 import com.spring.SpringBootMVC.model.UserModel;
 
+@Service
 public interface UserRepository extends CrudRepository<UserModel, Integer>{
 
 }
